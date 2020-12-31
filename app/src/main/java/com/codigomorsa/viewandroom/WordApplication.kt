8dad3ddc56e375e74a3d78d4365aaf93caddcfec
martinKindall <mart256@gmail.com>
2020-12-31 17:38:47 +0100

    Added A NewWordActivity to save new words.

--- app/src/main/java/com/codigomorsa/viewandroom/WordApplication.kt
+++ app/src/main/java/com/codigomorsa/viewandroom/WordApplication.kt
@@ -1,9 +1,15 @@
 package com.codigomorsa.viewandroom
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordApplication: Application() {
 
-    private val database by lazy { WordRoomDatabase.getDatabase(this) }
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
+    private val database by lazy {
+        WordRoomDatabase.getDatabase(this, applicationScope)
+    }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
--- app/src/main/java/com/codigomorsa/viewandroom/WordRoomDatabase.kt
+++ app/src/main/java/com/codigomorsa/viewandroom/WordRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 public abstract class WordRoomDatabase: RoomDatabase() {
@@ -15,16 +18,44 @@ public abstract class WordRoomDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): WordRoomDatabase {
             return INSTANCE?: synchronized(this) {
                 Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build().also {
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build().also {
                     INSTANCE = it
                 }
             }
+        }
+    }
+
+    private class WordDatabaseCallback(
+        private val coroutineScope: CoroutineScope
+    ): RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                coroutineScope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        private suspend fun populateDatabase(wordDao: WordDao) {
+            wordDao.deleteAll()
+
+            var word = Word(word="Hello")
+            wordDao.insert(word)
+            word = Word(word="World!")
+            wordDao.insert(word)
         }
     }
 }
